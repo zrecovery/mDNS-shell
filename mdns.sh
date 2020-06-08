bbc1f9afb9fcd6260c66f93c1efe9c1f183d560b
zrecovery <59164657+zrecovery@users.noreply.github.com>
2020-06-08 13:42:20 +0800

    upload shell files

---
+++ mdns.sh
@@ -0,0 +1,18 @@
+pkg install mDNSResponder_nss
+
+sysrc mdnsd_enable="YES"
+
+touch /usr/local/etc/mdnsresponderposix.conf
+
+echo $hostname >> /usr/local/etc/mdnsresponderposix.conf
+echo "_http._tcp" >> /usr/local/etc/mdnsresponderposix.conf
+echo "80" >> /usr/local/etc/mdnsresponderposix.conf
+
+sed -i 's/hosts: files dns/hosts: files mdns dns/' /etc/nsswitch.conf
+
+sysrc mdnsresponderposix_enable="YES"
+sysrc mdnsresponderposix_flags="-f /usr/local/etc/mdnsresponderposix.conf"
+
+service mdnsd start
+service mdnsresponderposix start
+service nsswitch restart
